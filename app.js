6b6162e79bd843ead74f83d07a1a6f747bab82d8
Simon McManus <mcmanus.simon@gmail.com>
2012-07-28 18:50:47 +0100

    sdf

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ twit.stream('statuses/sample', function(stream) {
 });
 
 
-app.engine('.ejs', ejs);
+app.engine('.ejs', ejs, function() {});
 app.get('/', function(req, res, next) {
     res.render('olympics.ejs', {
         events: events,
